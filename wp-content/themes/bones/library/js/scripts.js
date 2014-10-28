a0a37d61c790c4aad5c277ebc3fcc3545046343d
Wen Zhang <wen@click3x.com>
2014-10-28 11:20:52 -0400

    push

--- wp-content/themes/bones/library/js/scripts.js
+++ wp-content/themes/bones/library/js/scripts.js
@@ -129,7 +129,7 @@ jQuery(document).ready(function($) {
 
 // FREE MASON
     $('.thumb-wrapper').freeMason({
-    contPadding : 0,     // Horizontal padding on container
+    contPadding : 20,     // Horizontal padding on container
     thumbGutter : 15,     // Padding on thumbnails
     speed       : '0.5s', // Animation speed
     maxCols     : 3       // Maximum # of columns
