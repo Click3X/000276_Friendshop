1e86bc50e909b17e721e5d244b2dcc128d5bb45a
Charles Howard Davis III <charles@click3x.com>
2014-10-29 16:43:00 -0400

    Took out fast click that was causing errors

--- wp-content/themes/bones/library/bones.php
+++ wp-content/themes/bones/library/bones.php
@@ -173,8 +173,7 @@ function bones_scripts_and_styles() {
 		// wp_register_script( 'freewall-js', get_stylesheet_directory_uri() . '/library/js/libs/freewall.js', array( 'jquery' ), '', true );
 
         // FAST CLICK
-        // wp_register_script( 'fastclick', get_stylesheet_directory_uri() . '/library/js/libs/fastclick.js', array( '' ), '', true );
-        wp_register_script( 'fastclick', get_stylesheet_directory_uri() . '/library/js/libs/fastclick.js', array( 'jquery' ), '', true );
+        // wp_register_script( 'fastclick', get_stylesheet_directory_uri() . '/library/js/libs/fastclick.js', array( 'jquery' ), '', true );
 
 		// enqueue styles and scripts
 		wp_enqueue_script( 'bones-modernizr' );
@@ -195,7 +194,7 @@ function bones_scripts_and_styles() {
 		*/
 
 		wp_enqueue_script( 'jquery' );
-        wp_enqueue_script( 'fastclick' );
+        // wp_enqueue_script( 'fastclick' );
 		wp_enqueue_script( 'bones-js' );
 		wp_enqueue_script( 'freemason.min' );
 		wp_enqueue_script( 'fancybox-js' );
