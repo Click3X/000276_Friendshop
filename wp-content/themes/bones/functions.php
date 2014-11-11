4878f216329bba62702a814d3811cb3d955bca85
Charles Howard Davis III <charles@click3x.com>
2014-11-11 12:08:05 -0500

    FIXED firefox background position issues in footer

--- wp-content/themes/bones/functions.php
+++ wp-content/themes/bones/functions.php
@@ -220,4 +220,30 @@ function helper($var) {
 }
 
 
+// ENABLE FULL TOOL BAR ON WYSIWYG EDITOR so that we can see WIREDRIVE DETAILS
+// function enable_more_buttons($buttons) {
+
+//   $buttons[] = 'fontselect';
+//   $buttons[] = 'fontsizeselect';
+//   $buttons[] = 'styleselect';
+//   $buttons[] = 'backcolor';
+//   $buttons[] = 'newdocument';
+//   $buttons[] = 'cut';
+//   $buttons[] = 'copy';
+//   $buttons[] = 'charmap';
+//   $buttons[] = 'hr';
+//   $buttons[] = 'visualaid';
+
+//   return $buttons;
+// }
+// add_filter("mce_buttons_3", "enable_more_buttons");
+
+// add_filter( 'tiny_mce_before_init', 'myformatTinyMCE' );
+// function myformatTinyMCE( $in ) {
+
+// $in['wordpress_adv_hidden'] = FALSE;
+
+// return $in;
+// }
+
 /* DON'T DELETE THIS CLOSING TAG */ ?>
